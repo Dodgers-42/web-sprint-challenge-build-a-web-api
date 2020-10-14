50eb4cb57036ea451b54255893dcb782f4dfa582
Dodger-42 <kevin.bing.barber@gmail.com>
2020-10-14 13:42:48 -0700

    fixed actoinRoute, got it to work!!

--- data/middleware/actionRoute.js
+++ data/middleware/actionRoute.js
@@ -1,45 +1,76 @@
 const express = reqire("express")
-
-const actionModel = require("../helpers/actionModel")
-
-const {validateActionId, validateActionCreation, validationProjectIDForAction} = require("./validations")
-
 const router = express.Router();
 
-router.get('/actions', (req, res, next) =>{
-    actionModel.get()
-        .then(action =>{
-            res.status(200).json(action)
+const projectDb = require("../helpers/projectModel");
+const db = require("../helpers/actionModel");
+const { route } = require("../middleware/projectRoute");
+
+// const {validateActionId, validateActionCreation, validationProjectIDForAction} = require("./validations")
+
+
+router.get('/', (req, res) =>{
+    db.get()
+        .then(actions => {
+            res.status(200).json(actions)
         })
-        .catch(next)
+        .catch(err => {
+            res.staus(500).json({message: "An error has occured"});
+        })
 })
 
-router.get('/actions:id', validateActionId, (req, res, next) =>{
-            res.status(200).json(action)
-})
-
-router.post('/actions', validationProjectIDForAction, validateActionCreation, (req, res, next) =>{
-    actionModel.insert(req.body)
-    .then(action =>{
-        res.status(201).json(action)
+router.post('/',async (req, res) => {
+    const {project_id, description, notes} = req.body;
+    if(!project_id || !description || !notes) 
+    return res.status(400).json({message: "project id, and notes required"});
+    try {
+        const project = await projectDb.get(project_id);
+        if(!project) res.srtatus(400).json({message: "project doesn't exist"});
+    }catch(err){
+        res.status(500).json({message: "An error has occured"});
+    }
+    db.insert(req.body)
+    .then(newAction => {
+        res.status(201).json(newAction)
     })
-    .catch(next)
+    .catch(err =>{
+        res.status(500).json({message: "An error has occured"});
 })
+});
 
-router.put('/actions/:id', validateActionId, validationProjectIDForAction, validateActionCreation, (req, res,next) =>{
-    actionModel.update(req.params.id, req.body)
+router.get('/:id', (req, res) =>{
+    dB.get(req.params.id)
     .then(action =>{
-        res.status(201).json(action)
+        if(action == 0)
+         return res.status(404).json({message: "Action does not exist"})
+        res.status(200).json(action);
     })
-    .catch(next)
-})
+    .catch(err => {
+        res.status(500).json({message: "An error has occured"});
+    })
+});
 
-router.delete('/actions/:id', validateActionId, (req, res, next) =>{
-    actionModel.remove(req.params.id)
-    .then(action =>{
-        res.status(201).json(action)
+
+router.put('/:id', (req, res) =>{
+    const changes = req.body;
+    dB.update(req.params.id, changes)
+    .then(updatedAction =>{
+        if(!updateedAction) return res.status(404).json({message: "Action does not exist"})
+        res.status(200).json(updatedAction);
     })
-    .catch(next)
+    .catch(err => {
+        res.status(500).json({message: "An error has occured"});
+    });
+});
+
+router.delete('/:id', (req, res) =>{
+    dB.remove(req.params.id)
+    .then(deleteAction =>{
+        if(deleteAction == 0) return res.status(404).json({message: "Action does not exist"})
+        res.status(200).json({message: `Action with id ${req.params.id} deleted`});
+    })
+    .catch(err => {
+        res.status(500).json({message: "An error has occured"});
+    })
 })
 
 
